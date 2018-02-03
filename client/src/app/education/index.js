6ca4ba98990ea8ab329cd5f99a45f848f79d85f2
David Kajpust <kajpustd@gmail.com>
2018-02-03 16:58:36 -0400

    bootstrap cdn inserted

--- client/src/app/education/index.js
+++ client/src/app/education/index.js
@@ -4,7 +4,19 @@ export default class Education extends Component {
   render() {
     return (
       <div>
-        <h1>Education</h1>
+        <h1>Edu-fucking-cation</h1>
+        <form>
+          <li className="col-xs-12" >
+            <span className="col-xs-12 col-sm-5 col-md-5 col-lg-5">
+              <div className="col-xs-2">Checkmark</div>
+              <div className="col-xs-10">Description</div>
+            </span>
+          </li>
+          <span className="col-xs-12">
+            <button type="submit" value="Submit" className="btn btn-warning">Back</button>
+            <button type="submit" value="Submit" className="btn btn-secondary">Submit</button>
+          </span>
+        </form>
       </div>
     );
   }
