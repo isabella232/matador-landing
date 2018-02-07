cd31a614dde0d975b7132720cc79a3566cf28575
Greg Markou <gregorymarkou@gmail.com>
2018-02-07 09:31:50 -0500

    Restructure routes

--- server/routes/company.js
+++ server/routes/company.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/api/users', function(req, res, next) {
+router.post('/api/users', function(req, res, next) {
   res.status(200).json('user api');
 });
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,12 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
+
+// Route Imports
+const company = require('./company');
+const tokens = require('./tokens');
+
+// Routes
+router.use('/company', company);
+router.use('/books', books);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
