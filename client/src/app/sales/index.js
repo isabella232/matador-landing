3a1765d93a937e1db70014a27a6a55759d655d52
Greg Markou <gregorymarkou@gmail.com>
2018-02-07 15:37:02 -0500

    Update from master

--- client/src/app/sales/index.js
+++ client/src/app/sales/index.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SalesPage extends Component {
+
+  componentWillMount = () => {};
+  
   render() {
     return (
       <div>
